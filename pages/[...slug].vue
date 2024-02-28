35f19cfe2e7a041b4983c2f659cb4ff6b03861bb
panduola666 <panduola666@gmail.com>
2024-02-29 01:57:14 +0800

    feat: 404頁面

---
+++ pages/[...slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-circle min-vh-100 d-flex align-items-center justify-content-center">
+    <div class="d-flex align-items-center gap-3">
+      <img src="../assets/img/common/LogoIcon.png" alt="">
+      <div class="text-center">
+        <h1 class="title fw-bold">404</h1>
+        <div class="d-flex align-items-center gap-5">
+          <p class="h3 fw-bold">找不到此頁面</p>
+          <nuxt-link to="/" class="btn btn-lg btn-secondary">回首頁</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+// onMounted(() => {
+//   useRouter().push('/')
+// })
+</script>
+
+<style lang="scss" scoped>
+.bg-circle{
+  background: radial-gradient(circle, $primary, $secondary);
+}
+.title {
+  font-size: 8rem;
+}
+</style>
