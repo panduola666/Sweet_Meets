adad7fda725815bb3c2d5db6d96783d2d543ba39
panduola666 <panduola666@gmail.com>
2024-03-04 00:34:37 +0800

    feat: meta 設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,8 +26,26 @@ export default defineNuxtConfig({
   },
   app:{ // SEO and Meta
     head: {
-      charset: 'utf-8',
+      charset: 'UTF-8',
       viewport: 'width=device-width, initial-scale=1',
+      htmlAttrs: {
+        lang: 'zh-TW'
+      },
+      meta:[
+        { name: 'keywords', content: 'DIY甜點,自己做甜點,台北DIY' },
+        { name: 'title', content: 'DIY甜點俱樂部|相遇甜點' },
+        { name: 'description', content: '想自己DIY動手做甜點，無奈做一次需要用到各種材料及用具嗎？這裡已經把需要的材料用具全部備齊，還等甚麼？趕快來報名吧！預約時段：12:00-21:00，地址：(100)台北市中正區鄭州路8號' },
+        { name: 'og:title', content: 'DIY甜點俱樂部|相遇甜點' },
+        { name: 'og:description', content: '想自己DIY動手做甜點，無奈做一次需要用到各種材料及用具嗎？這裡已經把需要的材料用具全部備齊，還等甚麼？趕快來報名吧！預約時段：12:00-21:00，地址：(100)台北市中正區鄭州路8號' },
+        { name: 'og:image', content: 'https://github.com/panduola666/Sweet_Meets/blob/master/assets/img/Logo.png?raw=true' },
+        { name: 'og:url', content: 'https://sweetmeets.onrender.com' },
+        { name: 'og:site_name', content: '相遇甜點' },
+        { name: 'og:locale', content: 'zh_TW' },
+        { name: 'og:type', content: 'website' },
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'robots', content: 'index,follow' },
+        { name: 'author', content: '圈圈' },
+      ]
     }
   },
   experimental: {
